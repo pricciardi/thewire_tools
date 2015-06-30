2ed1fbb80d30a8a3cd08b509a1f39526cb96c1dd
pricciardi <pietro.ricciardi@gmail.com>
2015-07-01 01:39:19 +0200

    Create it.php

---
+++ languages/it.php
@@ -0,0 +1,58 @@
+<?php
+return array(
+
+	/**
+	 * Menu items and titles
+	 */
+	'thewire' => "Il Post",
+	'thewire:everyone' => "Tutti i post",
+	'thewire:user' => "I post di %s",
+	'thewire:friends' => "Post degli amici",
+	'thewire:replying' => "Risposta a %s (@%s) che ha scritto",
+	'thewire:thread' => "Thread",
+	'thewire:charleft' => "caratteri rimanenti",
+	'thewire:tags' => "Post con il tag '%s'",
+	'thewire:noposts' => "Ancora nessun post",
+	'item:object:thewire' => "post",
+	'thewire:by' => 'Post di %s',
+	'thewire:previous:help' => "Vedi post precedente",
+	'thewire:hide:help' => "Nascondi post precedente",
+
+	/**
+	 * The wire river
+	 */
+	'river:create:object:thewire' => "%s ha scritto %s",
+	'thewire:wire' => 'il post',
+
+	/**
+	 * Wire widget
+	 */
+	'thewire:widget:desc' => 'Mostra i tuoi ultimi post',
+	'thewire:num' => 'Numero di post da mostrare',
+	'thewire:moreposts' => 'Altri post',
+
+	/**
+	 * Status messages
+	 */
+	'thewire:posted' => "Post inserito.",
+	'thewire:deleted' => "Post cancellato.",
+	'thewire:blank' => "Spiacente, devi scrivere qualcosa per poterlo postare.",
+	'thewire:notfound' => "Spiacente, non riusciamo a trovare il post che cerchi.",
+	'thewire:notsaved' => "Spiacente. non riusciamo a salvare questo post.",
+	'thewire:notdeleted' => "Spiacente. non riusciamo a cancellare questo post.",
+
+	/**
+	 * Notifications
+	 */
+	'thewire:notify:summary' => 'Nuovo post: %s',
+	'thewire:notify:subject' => "Nuovo poast di %s",
+	'thewire:notify:reply' => '%s ha risposto a %s con il post:',
+	'thewire:notify:post' => '%s ha postato:',
+	'thewire:notify:footer' => "Leggi e rispondi:\n%s",
+
+	/**
+	 * Settings
+	 */
+	'thewire:settings:limit' => "Numero massimo di caratteri per i post:",
+	'thewire:settings:limit:none' => "Nessun limite",
+);
